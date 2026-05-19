e54f34be2d852dc4cb16655f30c405cd07dd5b5a
Ratan Singh <ratansinghthakur28@gmail.com>
2026-05-19 15:59:20 +0530

    i fixed the scroll issues.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "@/components/ScrollToTop";
 import { Layout } from "@/components/layout/Layout";
 import { CartProvider } from "@/contexts/CartContext";
 import { CurrencyProvider } from "@/contexts/CurrencyContext";
@@ -48,6 +49,7 @@ const App = () => (
               v7_relativeSplatPath: true,
             }}
           >
+            <ScrollToTop />
             <Layout>
               <Routes>
                 <Route path="/" element={<HomePage />} />
